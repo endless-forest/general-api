952155b9917c297c7163c05d7da6fb599cfc29a0
endless_forest <ingrey1@gmail.com>
2024-01-16 15:52:08 -0800

    add cors for all routes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var createError = require('http-errors');
+const createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var mathRouter = require('./routes/math');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
