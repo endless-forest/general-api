88ae5bca4ff466fecda0c53e2fcefafd33f17fe4
endless_forest <ingrey1@gmail.com>
2024-01-16 16:10:31 -0800

    add endpoint to call Wolfram simple api

--- routes/math.js
+++ routes/math.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const axios = require('axios')
+const router = express.Router();
+
+const apiKey = ""
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('wolfram math api');
+router.get('/', async function(req, res, next) {
+  const query = "2 + 2"
+  const fullUrl = `https://api.wolframalpha.com/v1/simple?appid=${apiKey}&i=${query}`
+  const results = await axios.get(fullUrl) 
+  console.info(results)
+  res.send(results.data);
 });
 
 module.exports = router;
