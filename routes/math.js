84a4009241982152cb3adf0884b76da7731940a1
endless_forest <ingrey1@gmail.com>
2024-01-18 22:16:16 -0800

    add env api key

--- routes/math.js
+++ routes/math.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios')
 const router = express.Router();
 
-const apiKey = ""
+const apiKey = process.env.WOLF_API_KEY
 
 /* GET query math api. */
 router.get('/', async function(req, res, next) {
