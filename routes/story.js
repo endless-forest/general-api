a7eb85c95eb64830c1a3d9ac960d95c55ee26dfc
endless_forest <ingrey1@gmail.com>
2024-02-20 13:01:08 -0800

    update story

--- routes/story.js
+++ routes/story.js
@@ -1,27 +1,25 @@
 const express = require("express");
-var cors = require('cors')
 const router = express.Router();
 
-const apiKey =  process.env.STORY_API_KEY
-
-async function postData(url = "", data = {}) {
-  const response = await fetch(url, {
-    method: "POST", 
-    mode: "cors", 
-    cache: "no-cache", 
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    redirect: "follow", 
-    referrerPolicy: "no-referrer", 
-    body: JSON.stringify(data), 
-  });
-  return response.json()
-}
+const apiKey = process.env.STORY_API_KEY
+  async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  };
 
 /* get /story api. */
-router.get("/", cors(), async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   const prompt = req.query.prompt;
   const fullUrl = `https://api.openai.com/v1/chat/completions`;
   const storyData = {
@@ -31,12 +29,14 @@ router.get("/", cors(), async function (req, res, next) {
   let results;
   try {
     const response = await postData(fullUrl, storyData);
-    results = { story: response.choices[0].message.content }
+    results = response.choices
+      ? { story: response.choices[0].message.content }
+      : { story: "" };
   } catch (error) {
     console.info("error", error);
     results = { error: error };
   }
-
+  res.set("Access-Allow-Origin", "*");
   res.send(results);
 });
 
